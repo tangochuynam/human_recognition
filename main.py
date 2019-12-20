d3a192cdd869c4f0bf570191cd305bf435ea111f
Vinh Vo <vqvinh243@gmail.com>
2019-12-20 19:01:04 +0900

    switch model

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ parser.add_argument('--video', help='Path to video file.')
 args = parser.parse_args()
 
 # imported related models
-estimator = load_pretrain_model('VGG_origin')
-# estimator = load_pretrain_model('mobilenet_thin')
+# estimator = load_pretrain_model('VGG_origin')
+estimator = load_pretrain_model('mobilenet_thin')
 action_classifier = load_action_premodel('Action/training/amazon_recognition.h5')
 
 # parameter initialization
-realtime_fps = '0.0000'
+realtime_fps = 0.0000
 start_time = time.time()
 fps_interval = 1
 fps_count = 0
